bbd60808e065c92f954f71e3e65e9e265fba6346
techexplorers123 <73522283+techexplorers123@users.noreply.github.com>
2023-03-26 14:50:06 +0530

    fixed the move() function and download() function on libmpv_win_downloader.py (#91)
    * fixed the move() function and download() function
    
    * fixed some bugs with the ttsdk_downloader.py and downloader.py
    
    added user agent string to the request header. now, it will not trigger error 406 event.
    added error handling in downloader.py

--- tools/libmpv_win_downloader.py
+++ tools/libmpv_win_downloader.py
@@ -21,8 +21,7 @@ url = "https://sourceforge.net/projects/mpv-player-windows/files/libmpv/"
 
 def download():
     try:
-        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
-        r = requests.get(url, headers=headers)
+        r = requests.get(url)
         r.raise_for_status() # raise an error if there was a problem with the request
     except requests.exceptions.RequestException as e:
         print(f"Error: {e}")
@@ -32,10 +31,10 @@ def download():
     table = page.find("table")
     
     if platform.architecture()[0][0:2] == "64":
-        l_ver = table.find("a", href=True, title=re.compile("x86_64")).get("title")
+        download_url = table.find("a", href=True, title=re.compile("x86_64")).get("href")
     else:
-        l_ver = table.find("a", href=True, title=re.compile("i686")).get("title")
-    download_url = l_ver.replace("Click to download ", "https://excellmedia.dl.sourceforge.net/project/mpv-player-windows/libmpv/")
+        download_url = table.find("a", href=True, title=re.compile("i686")).get("href")
+    
     try:
         downloader.download_file(download_url, os.path.join(os.getcwd(), "libmpv.7z"))
     except Exception as e:
@@ -57,15 +56,16 @@ def extract():
         print(f"Error extracting file: {e}")
         return
 
-def move_file():
+def move():
     try:
         source = os.path.join(os.getcwd(), "libmpv", "libmpv-2.dll")
-        dest = os.path.join(os.getcwd(), os.pardir) if os.path.basename(os.getcwd()) == "tools" else os.getcwd()
+        dest = os.path.join(os.getcwd(), "mpv.dll")
         if not os.path.exists(source):
-            raise FileNotFoundError("The file libmpv-2.dll does not exist")
-        elif os.path.exists(os.path.join(dest, "libmpv-2.dll")):
-            os.remove(os.path.join(dest, "libmpv-2.dll"))
-        shutil.move(source, os.path.join(dest, "libmpv-2.dll"))
+            raise FileNotFoundError("The file mpv-2.dll does not exist")
+            return
+        elif os.path.exists(dest):
+            os.remove(dest)
+        os.rename(source, dest)
     except (FileNotFoundError, FileExistsError, Exception) as e:
         print(f"Error moving file: {e}")
 
@@ -73,23 +73,22 @@ def clean():
     os.remove(os.path.join(os.getcwd(), "libmpv.7z"))
     shutil.rmtree(os.path.join(os.getcwd(), "libmpv"))
 
+
 def install():
     if sys.platform != "win32":
         sys.exit("This script should be run only on Windows")
-    print("Installing libmpv for Windows...")
-    print("Downloading latest libmpv version...")
+    print("Installing libmpv for Windows")
+    print("Downloading latest libmpv version")
     download()
-    print("Downloaded")
-    print("extracting...")
+    print("Downloaded. extracting")
     extract()
-    print("extracted")
-    print("moving...")
-    move_file()
-    print("moved")
-    print("cleaning...")
+    print("Extracted. moving")
+    move()
+    print("moved. cleaning")
     clean()
     print("cleaned.")
-    print("Installed, exiting.")
+    print("Installed")
+
 
 if __name__ == "__main__":
     install()
